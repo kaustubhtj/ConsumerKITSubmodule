2b4bd072fbd2ae9c7d336c400ace89d18f5f0c32
Kaustubh Kushte <kaustubhkushte@gmail.com>
2018-10-30 14:29:23 +0530

    Account View Controller Created

---
+++ Account/AccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AccountViewController.swift
+//  Container
+//
+//  Created by Kaustubh Kushte on 30/10/18.
+//  Copyright © 2018 KK. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
