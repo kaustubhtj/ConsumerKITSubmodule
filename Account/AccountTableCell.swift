fd1ec9e6d84b56bee06e1ea1e248c2939536c705
Kaustubh Kushte <kaustubhkushte@gmail.com>
2018-10-30 14:30:51 +0530

    Cell created

---
+++ Account/AccountTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountTableCell.swift
+//  Container
+//
+//  Created by Kaustubh Kushte on 30/10/18.
+//  Copyright © 2018 KK. All rights reserved.
+//
+
+import UIKit
+
+class AccountTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
